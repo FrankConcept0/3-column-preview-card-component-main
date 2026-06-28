953884d2fad98dc09716c02d0455d8ff3830b443
FrankConcept0 <ezikeiheomafranklinuche@gmail.com>
2026-06-28 17:46:19 +0100

    3-Column-Preview-Card

---
+++ script.js
@@ -0,0 +1,17 @@
+// let sedans = document.getElementById("learn-suv");
+// let suv = document.getElementById("learn-suv");
+// // let luxury = document.getElementById("learn-luxury");
+
+function learnSedan() {
+  //   window.open("https://en.wikipedia.org/wiki/Sedan_(automobile)");
+  console.log("me");
+}
+
+function learnSuv() {
+  window.open("https://en.wikipedia.org/wiki/SUV");
+  console.log(suv);
+}
+
+function learnLuxury() {
+  window.open("https://en.wikipedia.org/wiki/Luxury_car");
+}
